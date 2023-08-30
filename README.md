0c9ace3722bb0fe7bc5ff3317ca7e64e3d4eb9d5
Bill Glesias <bglesias@gmail.com>
2023-08-30 10:36:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# To reproduce
+
+docker run -it -v $PWD:/e2e -w /e2e cypress/included:12.17.4
---
+++ cypress/support/commands.js
@@ -0,0 +1,5 @@
+import 'cypress-plugin-stripe-elements';
+import 'cypress-iframe';
+import "cypress-real-events";
+import registerCypressGrep from '@cypress/grep/src/support'
+registerCypressGrep()
